76ae1716d14a798c7679a50ab0c269da4178002f
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2026-01-29 22:27:25 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
+
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
-export const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { Tabs } from './components/Tabs/Tabs';
 
 export const App = () => (
   <div className="section">
@@ -15,29 +11,7 @@ export const App = () => (
 
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="TabContent">
-        Some text 1
+        <Tabs />
       </div>
     </div>
   </div>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,39 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+import { useState } from 'react';
+
+export const Tabs = ({ tabs }) => {
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+
+  return (
+    <>
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            className={classNames({ 'is-active': tab.id === activeTabId })}
+          >
+            <a
+              href={`#${tab.id}`}
+              onClick={e => {
+                e.preventDefault();
+                setActiveTabId(tab.id);
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div>
+        {tabs.map(
+          tab =>
+            tab.id === activeTabId && (
+              <div key={tab.id} id={tab.id}>
+                {tab.content}
+              </div>
+            ),
+        )}
+      </div>
+    </>
+  );
+};
